1cf317329a451550b5f97b0c75fd6d8b5fc5fa06
Sergei Biryukov <svbiryukov@luxoft.com>
2025-01-10 00:40:02 +0100

    Comment out Azure resource group, service plan, and Linux web app definitions in main.tf

--- main.tf
+++ main.tf
@@ -24,25 +24,25 @@ resource "random_string" "random" {
     special = false
 }
 
-resource "azurerm_resource_group" "group" {
-  name = random_string.random.result
-  location = "East US"
-}
+# resource "azurerm_resource_group" "group" {
+#   name = random_string.random.result
+#   location = "East US"
+# }
 
-resource "azurerm_service_plan" "service_plan" {
-  name = random_string.random.result
-  location = azurerm_resource_group.group.location
-  resource_group_name = azurerm_resource_group.group.name
-  os_type = "Linux"
-  sku_name = "F1"
-}
+# resource "azurerm_service_plan" "service_plan" {
+#   name = random_string.random.result
+#   location = azurerm_resource_group.group.location
+#   resource_group_name = azurerm_resource_group.group.name
+#   os_type = "Linux"
+#   sku_name = "F1"
+# }
 
-resource "azurerm_linux_web_app" "web_app" {
-  name = random_string.random.result
-  location = azurerm_resource_group.group.location
-  resource_group_name = azurerm_resource_group.group.name
-  service_plan_id = azurerm_service_plan.service_plan.id
-  site_config {
-    always_on = false
-  }
-}
+# resource "azurerm_linux_web_app" "web_app" {
+#   name = random_string.random.result
+#   location = azurerm_resource_group.group.location
+#   resource_group_name = azurerm_resource_group.group.name
+#   service_plan_id = azurerm_service_plan.service_plan.id
+#   site_config {
+#     always_on = false
+#   }
+# }
